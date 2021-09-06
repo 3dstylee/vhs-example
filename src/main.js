84a0b7314db42fd135b3869a6fa545ad6b66ff00
Kensei Sakai <kensei.sakai@gmail.com>
2021-09-07 00:17:17 +0900

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 Vue.config.ignoredElements = [/^a-/];
 
 new Vue({
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
